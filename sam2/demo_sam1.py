7e6ba7eacf5bad2a712fdc8e3f6fb5709e64b351
shallbe1 <133226012+shallbe1@users.noreply.github.com>
2025-07-25 16:43:37 +0800

    Update demo_sam1.py

--- sam2/demo_sam1.py
+++ sam2/demo_sam1.py
@@ -4,27 +4,9 @@ from PIL import Image
 from sam2.build_sam import build_sam2
 from sam2.sam2_image_predictor import SAM2ImagePredictor
 import matplotlib.pyplot as plt
-import time
 from sam2.automatic_mask_generator import SAM2AutomaticMaskGenerator
 import cv2
-def show_anns(anns):
-    if len(anns) == 0:
-        return
-    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
-    ax = plt.gca()
-    ax.set_autoscale_on(False)
-    
-    img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 4))
-    img[:,:,3] = 0
 
-    for ann in sorted_anns:
-        #name=name+1
-        m = ann['segmentation']
-        color_mask = np.concatenate([np.random.random(3), [0.35]])
-        img[m] = color_mask
-        
-    ax.imshow(img)
-    
 
 def build_sam():
     device = "cuda" if torch.cuda.is_available() else "cpu"
